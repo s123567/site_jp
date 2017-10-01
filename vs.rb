6c478c1f333f8146a89f24737528a59f71ff4c69
Mathieu Chiron <mathieu_chiron@yahoo.com>
2017-10-01 21:00:35 +0200

    commit initial et ajout dernier nouvelle (magret)

---
+++ vs.rb
@@ -0,0 +1,11 @@
+QUESTIONS = ['question 1?', 'question 2?', 'question 3?']
+REPONSES = ['oui', 'bliblo', 'encore', 'peutetre', "c'est sur", "tant miuex"]
+
+random_q = QUESTIONS.shuffle.pop.to_s
+random_a = REPONSES.shuffle.pop.to_s
+
+puts "Cards vs humanity"
+puts "djhfbdjhfbjrhf"
+
+p random_q
+p random_a
